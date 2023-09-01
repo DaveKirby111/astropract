20f5dc3e9dda1962f6b71a783e4d013b4937292b
David Kirby <david@kinetic.com>
2023-09-01 15:08:47 -0500

    practice 2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        lime: "#00FF00",
+      },
+
+      fontFamily: {
+        rubik: ["Rubik Iso"],
+      },
+    },
+  },
+  plugins: [],
+};
